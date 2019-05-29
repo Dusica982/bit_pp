42db3a7fbe1dd3c0ce3d723f2e0bddba8b1fba38
BIT Student <classroom@bgit.rs>
2019-05-29 16:56:11 +0200

    geolocation and dog

--- Week6/w6d2/dog.js
+++ Week6/w6d2/dog.js
@@ -1,9 +1,21 @@
-var dog = new XMLHttpRequest();
-dog.open("GET", "https://dog.ceo/api/breeds/image/random");
-dog.send();
 
-dog.onload = function () {
+var inputEle = document.querySelector("#input");
 
-    var data = JSON.parse(dog.responseText);
-    console.log(data);
+inputEle.addEventListener("click", addNewPicture);
+
+
+function addNewPicture(event) {
+    var dog = new XMLHttpRequest();
+    dog.open("GET", "https://dog.ceo/api/breeds/image/random");
+    dog.send();
+
+    dog.onload = function () {
+        var data = JSON.parse(dog.response);
+        console.log(data);
+        var imgEle = document.createElement("img");
+        imgEle.setAttribute("src", data.message);
+        document.querySelector("body").appendChild(imgEle);
+    }
+
 }
+
